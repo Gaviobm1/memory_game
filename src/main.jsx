8dc878954aba1b9247dfd6c45601cc50ed00dfe1
Gavin O Brien <142741214+Gaviobm1@users.noreply.github.com>
2024-05-22 13:35:10 +0200

    Readd strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Game from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Game />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>
+);
